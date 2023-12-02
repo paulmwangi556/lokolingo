70cd26da54843140f2baa9baebd2f676223cc661
josephmusila <josemusila03@gmail.com>
2023-12-02 14:30:40 +0300

    commit

--- driploft/settings.py
+++ driploft/settings.py
@@ -1,7 +1,10 @@
 import os
+import mimetypes
+from pathlib import Path
 
+mimetypes.add_type("text/css",".css",True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -14,8 +17,8 @@ SITE_ID = 1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', 'bd34-102-215-77-162.ngrok-free.app','lokolingo.vivatechy.com']
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', 'bd34-102-215-77-162.ngrok-free.app','lokolingo.vivatechy.com']
+ALLOWED_HOSTS =['*']
 
 # Application definition
 
@@ -128,10 +131,13 @@ CSRF_TRUSTED_ORIGINS = ['https://bd34-102-215-77-162.ngrok-free.app']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+MEDIA_URL="/files/"
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from driploft.wsgi import application
+
 # sys.path.insert(0, os.path.dirname(__file__))
 
 
