19434f977fe1c0f55c6a42e1d4f388f5a90fba8e
joseph <josemusila03@gmail.com>
2023-12-29 17:24:00 +0300

    opted for reddis in channels

--- driploft/settings.py
+++ driploft/settings.py
@@ -86,12 +86,14 @@ WSGI_APPLICATION = 'driploft.wsgi.application'
 ASGI_APPLICATION = 'driploft.asgi.application'
 
 
-CHANNEL_LAYERS={
-    'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-    }
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
